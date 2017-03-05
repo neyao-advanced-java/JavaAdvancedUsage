2acedb395961d2ce3fb4e277e5852188b725ad73
姚能俊 <ynj@deepdata.com.cn>
2017-03-06 06:41:16 +0800

    thread mx bean example

---
+++ src/main/java/org/oursight/neyao/java/advanced/concurrent/ThreadMXBeanExample.java
@@ -0,0 +1,19 @@
+package org.oursight.neyao.java.advanced.concurrent;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+/**
+ * Created by DellPC on 2017/3/6.
+ */
+public class ThreadMXBeanExample {
+
+    public static void main(String[] args) {
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] threadInfo = bean.dumpAllThreads(false, false);
+        for (ThreadInfo info : threadInfo) {
+            System.out.println(info.getThreadId() +": " + info.getThreadName());
+        }
+    }
+}
