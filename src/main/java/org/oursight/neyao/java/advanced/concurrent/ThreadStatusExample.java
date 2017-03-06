ac266a02cb1913e26b3f84caaf2be261e332fc7c
姚能俊 <ynj@deepdata.com.cn>
2017-03-06 20:04:36 +0800

    thread status example

---
+++ src/main/java/org/oursight/neyao/java/advanced/concurrent/ThreadStatusExample.java
@@ -0,0 +1,62 @@
+package org.oursight.neyao.java.advanced.concurrent;
+
+/**
+ * Created by neyao on 2017/3/6.
+ */
+public class ThreadStatusExample {
+
+    public static void main(String[] args) {
+
+        new Thread(new TimeWaitingThread(), "TimeWaitingThread").start();
+        new Thread(new WaitingThread(), "WaitingThread").start();
+        new Thread(new BlockedThread(), "BlockedThread-1").start();
+        new Thread(new BlockedThread(), "BlockedThread-2").start();
+
+    }
+
+    static class TimeWaitingThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    static class WaitingThread implements Runnable {
+
+        @Override
+        public void run() {
+            while(true) {
+                synchronized (WaitingThread.class) {
+                    try {
+                        WaitingThread.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    static class BlockedThread implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (BlockedThread.class) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+    }
+}
