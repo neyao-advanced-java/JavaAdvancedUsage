802c4389c735c7070cf1935e9c7dd796ae0fc1e1
姚能俊 <ynj@deepdata.com.cn>
2017-03-21 19:43:14 +0800

    add more example

---
+++ src/main/java/org/oursight/neyao/java/advanced/variable/JavaVariables.java
@@ -0,0 +1,12 @@
+package org.oursight.neyao.java.advanced.variable;
+
+/**
+ * Created by neyao on 2017/3/21.
+ */
+public class JavaVariables {
+
+    public static void main(String[] args) {
+        byte b = 100;
+        System.out.println(b);
+    }
+}
